6174fae382a5b9cc6149bde59175a78ffe00f098
huyxuan264 <huyxuan264@gmail.com>
2026-08-11 04:39:32 +0000

    [WM] Script 'f/extraction_information/llm' deployed

--- f/extraction_information/llm.py
+++ f/extraction_information/llm.py
@@ -6,7 +6,7 @@ def call_llm(model_cfg, prompt):
         api_key=model_cfg["api_key"],
         base_url=model_cfg.get("base_url"),  
     )
-
+    print("MODEL CFG:", model_cfg)
     res = client.chat.completions.create(
         model=model_cfg["model"],
         messages=[{"role": "user", "content": prompt}],
