823017b953d89e25989438a5e206fae9bfa837ed
huyxuan264 <huyxuan264@gmail.com>
2026-08-11 02:31:09 +0000

    [WM] Script 'f/extraction_information/phieu_chi' deployed

--- f/extraction_information/phieu_chi.py
+++ f/extraction_information/phieu_chi.py
@@ -10,33 +10,25 @@ import json
 import re
 from datetime import datetime, timezone, timedelta
 from openai import OpenAI
-from typing import Optional, List, Union, Any
+from typing import Optional, List, Union, Any, Literal
 from pydantic import BaseModel, ConfigDict
 import base64
 VN_TZ = timezone(timedelta(hours=7))
 
-from f.extraction_information.utils import (
-    _normalize_text_value,
-    parse_input_text,
-    extract_json,
-    clean_extracted_data,
-    compute_accuracy_score,
-)
-from f.extraction_information.schema import PhieuChi
-from f.extraction_information.prompt import build_prompt
+
 from f.extraction_information.main import run_extraction
+from f.extraction_information.schema_class import PhieuChi
 
 
-def main(file_content: bytes):
-
+def main(
+    file_content,
+    user_prompt: Optional[str] = None,
+    model_name: Literal["llama", "qwen_vlm", "gpt", "deepseek"] = "qwen_vlm",
+):
     return run_extraction(
         file_content=file_content,
-        parse_input_text=parse_input_text,
-        build_prompt=build_prompt,
-        extract_json=extract_json,
-        clean_extracted_data=clean_extracted_data,
-        compute_accuracy_score=compute_accuracy_score,
-        document_type="Phiếu chi",
-        model="5CD-AI/Vintern-3B-R-beta",
-        schema=PhieuChi,
+        user_prompt=user_prompt,
+        model_name=model_name,
+        schema_class=PhieuChi,   
     )
+
