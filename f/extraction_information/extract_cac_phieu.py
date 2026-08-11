86fba5f20b45fdf38397dd66a030b837e968753d
huyxuan264 <huyxuan264@gmail.com>
2026-08-11 03:07:12 +0000

    [WM] Script 'f/extraction_information/extract_cac_phieu' deployed

--- f/extraction_information/extract_cac_phieu.py
+++ f/extraction_information/extract_cac_phieu.py
@@ -1,22 +1,19 @@
 import os
 import wmill
-from typing import Optional
+from typing import Literal, Optional
 from f.extraction_information.main_new import run_extraction
-
 
 def main(
     file_content,
     user_prompt: Optional[str] = None,
 
+    # dropdown model
+    model_name: Literal["llama", "qwen_vlm", "gpt", "deepseek"] = "qwen_vlm",
 
-    model_name: Optional[str] = None,
-    model_registry_url: str = "u/huyxuan264/models",
-    prompt_registry_url: str = "u/huyxuan264/prompts",
-):
+
+    ):
     return run_extraction(
         file_content=file_content,
         user_prompt=user_prompt,
         model_name=model_name,
-        model_registry_url=model_registry_url,
-        prompt_registry_url=prompt_registry_url,
     )
