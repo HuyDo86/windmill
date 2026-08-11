30413d86ae3d36a90336894d5bbba7425ca1bc60
huyxuan264 <huyxuan264@gmail.com>
2026-08-11 02:31:16 +0000

    [WM] Script 'f/extraction_information/extract' deployed

---
+++ f/extraction_information/extract.py
@@ -0,0 +1,18 @@
+import os
+import wmill
+from typing import Literal, Optional
+from f.extraction_information.main_new import run_extraction
+
+
+def main(
+    file_content,
+    user_prompt: Optional[str] = None,
+
+    # dropdown model
+    model_name: Literal["llama", "qwen_vlm", "gpt", "deepseek"] = "qwen_vlm",
+):
+    return run_extraction(
+        file_content=file_content,
+        user_prompt=user_prompt,
+        model_name=model_name,
+    )
