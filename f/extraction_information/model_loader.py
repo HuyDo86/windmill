1429e7cc363d475855748fd9a6eb00b3e4e964f4
huyxuan264 <huyxuan264@gmail.com>
2026-08-11 04:36:04 +0000

    [WM] Script 'f/extraction_information/model_loader' deployed

--- f/extraction_information/model_loader.py
+++ f/extraction_information/model_loader.py
@@ -3,18 +3,24 @@ import wmill
 from typing import cast
 
 
+
 def get_model_config(model_name: str | None):
     registry = cast(dict, wmill.get_resource("f/extraction_information/models"))
-    models = registry.get("models", {}) 
+
+    models = registry.get("models", {})
+
     # default
     if not model_name:
-        model_name = list(registry.keys())[0]
+        model_name = registry.get("default") or list(models.keys())[0]
 
-    resource_path = models.get(model_name)  
+    resource_path = models.get(model_name)
 
     if not resource_path:
-        raise ValueError(f"Model {model_name} not found")
-    print("MODEL NAME:", model_name)
+        raise ValueError(f"Model {model_name} not found in {models}")
 
-    registry = wmill.get_resource("f/extraction_information/models")
-    
+    model_cfg = wmill.get_resource(resource_path)
+
+    if not model_cfg:
+        raise ValueError(f"Resource {resource_path} returned None")
+
+    return model_cfg
