3812f7393f0657052a0fd703d5ec3919a71cd452
huyxuan264 <huyxuan264@gmail.com>
2026-08-12 07:28:10 +0000

    [WM] Script 'f/extraction_information/phieu_thue' deployed

--- f/extraction_information/phieu_thue.py
+++ f/extraction_information/phieu_thue.py
@@ -1,42 +1,16 @@
-# requirements:
-# openai==1.54.0
-# wmill
-# httpx==0.27.2
-# pydantic==2.9.0
-
 import wmill
-import os
-import json
-import re
-from datetime import datetime, timezone, timedelta
-from openai import OpenAI
-from typing import Optional, List, Union, Any
-from pydantic import BaseModel, ConfigDict
-import base64
-VN_TZ = timezone(timedelta(hours=7))
-
-from f.extraction_information.utils import (
-    _normalize_text_value,
-    parse_input_text,
-    extract_json,
-    clean_extracted_data,
-    compute_accuracy_score,
-)
-from f.extraction_information.schema import PhieuNopThue
-from f.extraction_information.prompt import build_prompt
-from f.extraction_information.main import run_extraction
+from typing import Optional
+from f.extraction_information.main_core import run_extraction
 
 
-def main(file_content: bytes):
-
+def main(
+    file_content: bytes,
+    prompt_path: Optional[str] = None,
+    model_path: Optional[str] = None,
+):
     return run_extraction(
         file_content=file_content,
-        parse_input_text=parse_input_text,
-        build_prompt=build_prompt,
-        extract_json=extract_json,
-        clean_extracted_data=clean_extracted_data,
-        compute_accuracy_score=compute_accuracy_score,
-        document_type="Phiếu thuế",
-        model="5CD-AI/Vintern-3B-R-beta",
-        schema=PhieuNopThue,
+        doc_type="phieu_thue",
+        model_path=model_path,
+        prompt_path=prompt_path,
     )
