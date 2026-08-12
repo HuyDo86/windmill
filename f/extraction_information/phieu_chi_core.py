aa925f7c63c9c90ce2be125aa0a99be1d9ce0c29
huyxuan264 <huyxuan264@gmail.com>
2026-08-12 07:20:45 +0000

    [WM] Script 'f/extraction_information/phieu_chi_core' deployed

---
+++ f/extraction_information/phieu_chi_core.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from f.extraction_information.main_core import run_extraction
+
+
+def main(
+    file_content: bytes,
+    prompt_path: Optional[str] = None,
+    model_path: Optional[str] = None,
+):
+    return run_extraction(
+        file_content=file_content,
+        doc_type="phieu_chi",
+        model_path=model_path,
+        prompt_path=prompt_path,
+    )
