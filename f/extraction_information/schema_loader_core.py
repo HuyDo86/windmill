3147b08fe3dc3dfd190a5b3dc487e378e97790af
huyxuan264 <huyxuan264@gmail.com>
2026-08-12 07:54:05 +0000

    [WM] Script 'f/extraction_information/schema_loader_core' deployed

--- f/extraction_information/schema_loader_core.py
+++ f/extraction_information/schema_loader_core.py
@@ -3,10 +3,20 @@ import wmill
 
 
 SCHEMA_REGISTRY = {
-    "phieu_chi": "f/extraction_information/phieu_chi",
-    "phieu_thu": "f/extraction_information/schema/phieu_thu",
-    "phieu_nhap": "f/extraction_information/schema/phieu_nhap",
-    "phieu_xuat": "f/extraction_information/schema/phieu_xuat",
+    "phieu_chi": "f/extraction_information/schema_phieu_chi",
+    "phieu_thu": "f/extraction_information/schema_phieu_thu",
+    "phieu_nhap": "f/extraction_information/schema_phieu_nhap",
+    "phieu_xuat": "f/extraction_information/schema_phieu_xuat",
+    "chuyen_khoan": "f/extraction_information/schema_chuyen_khoan",
+    "van_don": "f/extraction_information/schema_van_don",
+    "thanh_toan": "f/extraction_information/schema_thanh_toan",
+    "hoa_don": "f/extraction_information/schema_hoa_don",
+    "bao_gia": "f/extraction_information/schema_bao_gia",
+    "thue": "f/extraction_information/schema_thue",
+    "bien_ban_lam_viec": "f/extraction_information/schema_bien_ban_lam_viec",
+    "phieu_giao_nhan_hang_hoa": "f/extraction_information/schema_phieu_giao_nhan_hang_hoa",
+    "phieu_giao_nhan_be_tong": "f/extraction_information/schema_phieu_giao_nhan_be_tong",
+
 }
 
 
