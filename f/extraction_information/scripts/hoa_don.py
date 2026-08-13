06956b9e46079cfc5d7d31f923c45acf237ece63
huyxuan264 <huyxuan264@gmail.com>
2026-08-13 07:37:16 +0000

    [WM] Script 'f/extraction_information/scripts/hoa_don' deployed

--- f/extraction_information/scripts/hoa_don.py
+++ f/extraction_information/scripts/hoa_don.py
@@ -10,7 +10,7 @@ def main(
 ):
     return run_extraction(
         file_content=file_content,
-        doc_type="hoa don",
+        doc_type="hoa_don",
         model_path=model_path,
         prompt_path=prompt_path,
     )
